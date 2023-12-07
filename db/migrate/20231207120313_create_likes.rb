41f64c5100ad24ea52d0eef24e376787e126d2a1
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-07 18:44:35 +0100

    Generate likes migration file

---
+++ db/migrate/20231207120313_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :likes do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+    t.timestamps
+    end
+  end
+end

c3f33d6696f29449e3997f9982f8ec1155efafde
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-07 23:03:06 +0100

    Update migrate codes

--- db/migrate/20231207120538_create_comments.rb
+++ db/migrate/20231207120538_create_comments.rb
@@ -1,11 +1,10 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.bigint 'user_id'
       t.bigint 'post_id'
       t.text 'text'
-      t.index ['post_id'], name: 'index_comments_on_post_id'
-      t.index ['user_id'], name: 'index_comments_on_user_id'
+      t.references :user, foreign_key: true 
+      t.references :post, foreign_key: true
       t.timestamps
     end
   end

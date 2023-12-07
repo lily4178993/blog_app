ffdb769897c35bf24562237dcaebb951596a1a64
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-07 22:32:16 +0100

    create methods for models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_likes_counter(post)
+    post.likes_counter = post.likes_counter.to_i + 1
+    post.save!
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+
+  # update the posts counter for a user by 1
+  def update_posts_counter(user)
+    user.posts_counter = user.posts_counter.to_i + 1
+    user.save!
+  end
+
+  #  return 5 most recent comments for a given post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

ae03d54d8625bcb8da0d3ef341a03f0632698f5e
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-07 19:07:00 +0100

    Set up linters and fix linter errors

--- db/migrate/20231207120024_create_posts.rb
+++ db/migrate/20231207120024_create_posts.rb
@@ -1,13 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-    t.bigint "author_id"
-    t.string "title"
-    t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.index ["author_id"], name: "index_posts_on_author_id"
-    t.timestamps
+      t.bigint 'author_id'
+      t.string 'title'
+      t.text 'text'
+      t.integer 'comments_counter'
+      t.integer 'likes_counter'
+      t.index ['author_id'], name: 'index_posts_on_author_id'
+      t.timestamps
     end
   end
 end
--- db/migrate/20231207120538_create_comments.rb
+++ db/migrate/20231207120538_create_comments.rb
@@ -1,12 +1,12 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
-    t.text "text"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-    t.timestamps
+      t.bigint 'user_id'
+      t.bigint 'post_id'
+      t.text 'text'
+      t.index ['post_id'], name: 'index_comments_on_post_id'
+      t.index ['user_id'], name: 'index_comments_on_user_id'
+      t.timestamps
     end
   end
 end

ecf265fea33e5fc12511ba0c82df7923e46c3b2b
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-08 17:59:31 +0100

    Use after save rails active callback

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,11 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_likes_counter(post)
-    post.likes_counter = post.likes_counter.to_i + 1
-    post.save!
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
   end
 end

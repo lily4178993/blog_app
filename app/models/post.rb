8c24c6b2b9773ba995313d88db58629b393de464
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-08 18:00:11 +0100

    Use after save rails active callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,18 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  
   # update the posts counter for a user by 1
-  def update_posts_counter(user)
-    user.posts_counter = user.posts_counter.to_i + 1
-    user.save!
+  after_save :update_posts_counter
+
+  private 
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 
   #  return 5 most recent comments for a given post

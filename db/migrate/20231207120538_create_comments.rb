ac3c8ce3fafa63eca7e2c7114e45842387b0a27a
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-08 15:23:39 +0100

    Remove unnecessary codes in comment migration file

--- db/migrate/20231207120538_create_comments.rb
+++ db/migrate/20231207120538_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.bigint 'post_id'
       t.text 'text'
       t.references :user, foreign_key: true 
       t.references :post, foreign_key: true

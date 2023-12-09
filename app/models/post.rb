c24d45a7fcd4f724c6b0aebead627aaa9b127b5a
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-09 19:18:12 +0100

    Edit associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,24 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes
+  has_many :comments
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   # update the posts counter for a user by 1
   after_save :update_posts_counter
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter,presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  #  return 5 most recent comments for a given post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 
   private
 
   def update_posts_counter
     author.increment!(:posts_counter)
-  end
-
-  #  return 5 most recent comments for a given post
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    Rails.logger.debug
   end
 end

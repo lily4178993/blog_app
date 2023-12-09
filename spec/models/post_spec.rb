06e67783db8f51dd32a925d3ed91f4dd1ade9904
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-09 07:13:08 +0100

    Add test cases for posts model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe Post, type: :model do
+  describe '#recent_comments' do
+    let(:user) { User.create(name: 'Joy', photo: 'https://www.google.com/url', bio: 'she is a software developer', posts_counter: 0) }
+    let(:post) { Post.create!(author_id: user.id, title: 'Post 1', text: 'This is post 1', comments_counter: 0, likes_counter: 0) }
+
+    it 'returns 5 most recent comments for a post' do
+      6.times { |i| Comment.create(post: post, user: user, text: "Comment #{i + 1}") }
+
+      recent_comments = post.reload.comments.order(created_at: :desc).limit(5)
+
+      expect(recent_comments.count).to eq(5)
+      expect(recent_comments.first.text).to eq('Comment 6')
+      expect(recent_comments.last.text).to eq('Comment 2')
+    end
+  end
+end

e5547083ce64007c6ead23b43b6dc8801ca40ac8
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-16 18:07:29 +0100

    create actions for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
-class CommentController < ApplicationController
+class CommentsController < ApplicationController
+  before_action :set_user_and_post
+  
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = @post.comments.build(params.require(:comment).permit(:text).merge(user_id: current_user.id))
+    if @comment.save
+      flash[:success] = 'Comment saved successfully'
+      redirect_to user_post_path(@post, @user)
+    else
+      flash.now[:error] = 'Post not saved, try again later'
+      render :new
+    end
+  end
+
+  private
+
+  def set_user_and_post
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
   end
 end

2feee36671712fafdad607976139f83a3b8c7e39
Ayokunnumi1 <omololuayk@gmail.com>
2023-12-16 18:09:24 +0100

    create actions for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(params.require(:post).permit(:title, :text))
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_path
+    else
+      flash.now[:error] = 'Post not saved, try again later'
+      puts puts @post.errors.full_messages
+      render :new
+    end
+  end
 end

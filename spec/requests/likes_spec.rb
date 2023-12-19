fc67f67824ed256c5cca0fb1bfd4c0d2f131d6d9
lily4178993 <nellytelli@gmail.com>
2023-12-19 17:32:05 +0000

    feat: fix linter errors

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe LikesController, type: :request do
   describe 'POST /create' do
     let(:user) { User.create(name: 'John Doe', photo: 'path/to/photo.jpg', bio: 'Sample bio text') }
-    let(:post) { Post.create(
-      author: user,
-      title: 'Sample Title',
-      text: 'Sample post content'
-    ) }
+    let(:post) do
+      Post.create(
+        author: user,
+        title: 'Sample Title',
+        text: 'Sample post content'
+      )
+    end
 
     it 'creates a new like and redirects on success' do
       Like.create(user_id: user.id, post_id: post.id)
